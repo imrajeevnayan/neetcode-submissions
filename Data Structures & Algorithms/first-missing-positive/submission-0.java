18b0f8724e9936bfee0c09518a6db28041d4c7c5
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-05-21 21:15:27 +0530

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        int res=1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] ==res)res++;
+            else if (nums[i]> res) break;
+        }
+        return res;
+    }
+}
