3f081a32397f4007177de93601345e2b59dc187e
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-08-16 19:02:21 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] a = s.toCharArray();
+            Arrays.sort(a);
+            String key = new String(a);
+
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
